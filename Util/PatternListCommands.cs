34e60d3120e2866ed4fbaa8c4e32067809f82d87
Aivars Irmejs <aivars@gmail.com>
2025-03-09 17:36:20 +0200

    Added more default LED patterns

--- Util/PatternListCommands.cs
+++ Util/PatternListCommands.cs
@@ -28,6 +28,13 @@ public class PatternListCommands
         GetMagentaRunningWithOffLed,
         GetCyanRunningWithOffLed,
         GetWhiteRunningWithOffLed,
+        GetRedPulses,
+        GetGreenPulses,
+        GetBluePulses,
+        GetYellowPulses,
+        GetMagentaPulses,
+        GetCyanPulses,
+        GetWhitePulses,
     }
 
     public static readonly Dictionary<PatternName, PatternListItem?> PatternList = new()
@@ -193,7 +200,56 @@ public class PatternListCommands
                 new PatternLine(160, "FFFFFF", 10, 0),
                 new PatternLine(192, "FFFFFF", 10, 0),
             ])
-        }
+        },
+        {
+            PatternName.GetRedPulses, new PatternListItem("Red pulses",
+            [
+                new PatternLine(127, "FF0000", 150, 10),
+                new PatternLine(127, "110000", 150, 10),
+            ])
+        },
+        {
+            PatternName.GetGreenPulses, new PatternListItem("Green pulses",
+            [
+                new PatternLine(127, "00FF00", 150, 10),
+                new PatternLine(127, "001100", 150, 10),
+            ])
+        },
+        {
+            PatternName.GetBluePulses, new PatternListItem("Blue pulses",
+            [
+                new PatternLine(127, "0000FF", 150, 10),
+                new PatternLine(127, "000011", 150, 10),
+            ])
+        },
+        {
+            PatternName.GetYellowPulses, new PatternListItem("Yellow pulses",
+            [
+                new PatternLine(127, "FFFF00", 150, 10),
+                new PatternLine(127, "111100", 150, 10),
+            ])
+        },
+        {
+            PatternName.GetCyanPulses, new PatternListItem("Cyan pulses",
+            [
+                new PatternLine(127, "00FFFF", 150, 10),
+                new PatternLine(127, "001111", 150, 10),
+            ])
+        },
+        {
+            PatternName.GetMagentaPulses, new PatternListItem("Magenta pulses",
+            [
+                new PatternLine(127, "FF00FF", 150, 10),
+                new PatternLine(127, "110011", 150, 10),
+            ])
+        },
+        {
+            PatternName.GetWhitePulses, new PatternListItem("White pulses",
+            [
+                new PatternLine(127, "FFFFFF", 150, 10),
+                new PatternLine(127, "111111", 150, 10),
+            ])
+        },
     };
 
     public PatternListItem? PatternListByKey(PatternName var)
